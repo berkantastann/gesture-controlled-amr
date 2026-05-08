5599103b5d9d011642de65782bce1f0ab1507ebf
Berkan Tastan <btastan9@gmail.com>
2026-05-09 01:16:18 +0300

    controller_added

--- src/robot_description/launch/gazebo.launch.py
+++ src/robot_description/launch/gazebo.launch.py
@@ -1,15 +1,18 @@
 import os
+
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, RegisterEventHandler
+from launch.event_handlers import OnProcessExit
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 import xacro
 
+
 def generate_launch_description():
     pkg_share = get_package_share_directory('robot_description')
-    xacro_file = os.path.join(pkg_share, 'urdf', 'robot.urdf.xacro')
 
+    xacro_file = os.path.join(pkg_share, 'urdf', 'robot.urdf.xacro')
     robot_description_config = xacro.process_file(xacro_file)
     robot_description = {'robot_description': robot_description_config.toxml()}
 
@@ -17,7 +20,8 @@ def generate_launch_description():
 
     ign_gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')
+            os.path.join(get_package_share_directory('ros_gz_sim'),
+                         'launch', 'gz_sim.launch.py')
         ),
         launch_arguments={'gz_args': f'-r {world_file_path}'}.items(),
     )
@@ -26,25 +30,57 @@ def generate_launch_description():
         package='robot_state_publisher',
         executable='robot_state_publisher',
         output='screen',
-        parameters=[robot_description],
+        parameters=[robot_description, {'use_sim_time': True}],
     )
-
+    
     spawn_entity = Node(
         package='ros_gz_sim',
         executable='create',
-        arguments=['-topic', 'robot_description', '-name', 'my_robot', '-z', '0.1', "-x", "1.5"],
+        arguments=['-topic', 'robot_description',
+                   '-name', 'amr',
+                   '-x', '1.5', '-z', '0.1'],
         output='screen',
     )
-    
+
+    joint_state_broadcaster_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=['joint_state_broadcaster',
+                   '--controller-manager', '/controller_manager'],
+        output='screen',
+    )
+
+    diff_drive_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=['diff_drive_controller',
+                   '--controller-manager', '/controller_manager'],
+        output='screen',
+    )
+
+    lift_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=['lift_controller',
+                   '--controller-manager', '/controller_manager'],
+        output='screen',
+    )
+
+    cmd_vel_relay = Node(
+        package='topic_tools',
+        executable='relay',
+        arguments=['/cmd_vel', '/diff_drive_controller/cmd_vel_unstamped'],
+        parameters=[{'use_sim_time': True}],
+        output='screen',
+    )
+
     bridge = Node(
         package='ros_gz_bridge',
         executable='parameter_bridge',
         arguments=[
-            '/cmd_vel@geometry_msgs/msg/Twist]gz.msgs.Twist',
-            '/odom@nav_msgs/msg/Odometry[gz.msgs.Odometry',
-            '/tf@tf2_msgs/msg/TFMessage[gz.msgs.Pose_V',
-            '/joint_states@sensor_msgs/msg/JointState[gz.msgs.Model',
-            '/lift_cmd@std_msgs/msg/Float64]gz.msgs.Double',
+            # Sim saati
+            '/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock',
+            # Sensörler
             '/scan@sensor_msgs/msg/LaserScan[gz.msgs.LaserScan',
             '/imu@sensor_msgs/msg/Imu[gz.msgs.IMU',
             '/rgb_camera/image_raw@sensor_msgs/msg/Image[gz.msgs.Image',
@@ -54,7 +90,22 @@ def generate_launch_description():
             '/depth_camera/points@sensor_msgs/msg/PointCloud2[gz.msgs.PointCloudPacked',
             '/depth_camera/camera_info@sensor_msgs/msg/CameraInfo[gz.msgs.CameraInfo',
         ],
+        parameters=[{'use_sim_time': True}],
         output='screen',
+    )
+
+    after_spawn_jsb = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=spawn_entity,
+            on_exit=[joint_state_broadcaster_spawner],
+        )
+    )
+
+    after_jsb_diff_and_lift = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=joint_state_broadcaster_spawner,
+            on_exit=[diff_drive_spawner, lift_spawner],
+        )
     )
 
     return LaunchDescription([
@@ -62,4 +113,7 @@ def generate_launch_description():
         robot_state_publisher_node,
         spawn_entity,
         bridge,
+        cmd_vel_relay,
+        after_spawn_jsb,
+        after_jsb_diff_and_lift,
     ])
